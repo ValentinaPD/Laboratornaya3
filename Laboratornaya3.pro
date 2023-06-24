f55bba9aafbb6c3335a02fbc87cf711065be6f6c
Valya <valya.ponomarenko.00@mail.ru>
2023-06-24 21:09:04 +0300

    Тестовое подключение к б/д

--- Laboratornaya3.pro
+++ Laboratornaya3.pro
@@ -5,7 +5,7 @@
 #-------------------------------------------------
 
 QT       += core gui
-
+QT += sql
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 
 TARGET = Laboratornaya3
@@ -22,7 +22,6 @@ DEFINES += QT_DEPRECATED_WARNINGS
 # You can also select to disable deprecated APIs only up to a certain version of Qt.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
 
-
 SOURCES += \
         main.cpp \
         mainwindow.cpp
@@ -32,3 +31,5 @@ HEADERS += \
 
 FORMS += \
         mainwindow.ui
+
+DISTFILES +=
--- main.cpp
+++ main.cpp
@@ -1,11 +1,39 @@
 #include "mainwindow.h"
 #include <QApplication>
-
+#include <QtSql>
+#include <QTableView>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
-    w.show();
+
+    QSqlDatabase dbase = QSqlDatabase::addDatabase("QSQLITE");
+       dbase.setDatabaseName("C:\\TRPOLabs\\Laboratornaya3\\TEMPERATURE_NOVOSIB.sqlite");
+       if (!dbase.open()) {
+           qDebug() << "Что-то не так с соединением!";
+           return -1;
+       }
+
+       QTableView view;
+       QSqlTableModel model;
+
+       QList<QPair<QString, float_t>> data;
+
+       QStringList tables = dbase.tables();
+       QString table = tables.first();
+       auto query = QSqlQuery("SELECT * FROM " + table + " ");
+       while(query.next())
+       {
+          data.append(qMakePair(query.value(0).toString(), query.value(1).toFloat()));
+       }
+       model.setTable("my_table");
+       model.select();
+       model.setEditStrategy(QSqlTableModel::OnFieldChange);
+
+       view.setModel(&model);
+       view.show();
+
+    //w.show();
 
     return a.exec();
 }
