e7ff654fb4c2d5b4851e83d2f0103eb61ad6f03e
Valya <valya.ponomarenko.00@mail.ru>
2023-07-01 00:14:29 +0300

    фиксирую промежуточные изменения

--- datareader.h
+++ datareader.h
@@ -4,14 +4,30 @@
 #include <QtSql>
 
 class DataReader{
+public:
     DataReader();
+    virtual QString GetData(QString path);
+protected:
+    virtual bool IsOpen(QString path) = 0;
+    virtual bool IsEmpty(QString path) = 0;
+    virtual QString ReadData(QString path) = 0;
 
 };
-class SQLDataReader{
+class SQLDataReader : DataReader{
+private:
+    bool IsOpen(QString path);
+    bool IsEmpty(QString path);
+    QString ReadData(QString path);
+public:
     SQLDataReader(){};
     QList<QPair<QString, float_t>> GetData(QString path);
 };
-class JSONDataReader{
+class JSONDataReader : DataReader{
+private:
+    bool IsOpen(QString path);
+    bool IsEmpty(QString path);
+    QString ReadData(QString path);
+public:
     JSONDataReader(){};
     QList<QPair<QString, float_t>> GetData(QString path);
 };
